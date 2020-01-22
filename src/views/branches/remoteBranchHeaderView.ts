670fbad61130f7e2658e8bc973dae129feaead09
kahole <kristian@hole.priv.no>
2020-01-22 16:09:33 +0100

    comment

--- src/views/branches/remoteBranchHeaderView.ts
+++ src/views/branches/remoteBranchHeaderView.ts
@@ -5,7 +5,7 @@ export class RemoteBranchHeaderView extends TextView {
 
   constructor(name: string, upstreamRef: UpstreamRef) {
     super();
-    // TODO: ${upstreamRef.commit.message} PROPER COMMIT CACHE IMPLEMENTATION CAN SOLVE ALL OF THIS I THINK. GLOBAL CACHE?
+    // TODO: ${upstreamRef.commit.message} JUST FILL OUT THE MODEL A LITTLE MORE, WITH SOME COMMIT DETAILS. EASY ENOUGH
     // GitTextUtils.shortCommitMessage(
     this.textContent = `${name}: ${upstreamRef.remote}/${upstreamRef.name}`;
   }
