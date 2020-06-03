3660bfa3032b4610c54f81ae952a75f40ed4211d
Kristian Andersen Hole <kristian@hole.priv.no>
2020-06-03 20:14:59 +0200

    Makes helpView dynamic by parsing user keybindings (#24) (#37)
    * makes helpView dynamic by parsing user keybindings (#24)
    
    * makes helpview use 3 rows to avoid wrapping
    
    * adds '-' key to grammar

--- src/commands/helpCommands.ts
+++ src/commands/helpCommands.ts
@@ -1,12 +1,26 @@
 import { window, workspace } from 'vscode';
 import MagitUtils from '../utils/magitUtils';
 import { HelpView } from '../views/helpView';
-import { views } from '../extension';
+import { views, logPath } from '../extension';
 import { MagitRepository } from '../models/magitRepository';
+import * as path from 'path';
 
 export async function magitHelp(repository: MagitRepository) {
 
+  let keybindingsPath = path.join(logPath, '..', '..', '..', '..', 'User', 'keybindings.json');
+  let userKeyBindings = [];
+
+  try {
+    userKeyBindings = await workspace.openTextDocument(keybindingsPath).then(document =>
+      JSON.parse(
+        document
+          .getText()
+          .replace(/\/\*[\s\S]*?\*\/|\/\/.*$/gm, '')
+      )
+    );
+  } catch { }
+
   const uri = HelpView.encodeLocation(repository);
-  views.set(uri.toString(), new HelpView(uri));
+  views.set(uri.toString(), new HelpView(uri, userKeyBindings));
   workspace.openTextDocument(uri).then(doc => window.showTextDocument(doc, { viewColumn: MagitUtils.oppositeActiveViewColumn(), preserveFocus: true, preview: false }));
 }
--- src/views/helpView.ts
+++ src/views/helpView.ts
@@ -4,6 +4,7 @@ import * as Constants from '../common/constants';
 import { TextView } from './general/textView';
 import { MagitRepository } from '../models/magitRepository';
 import { MagitState } from '../models/magitState';
+import * as meta from '../../package.json';
 
 export class HelpView extends DocumentView {
 
@@ -11,49 +12,20 @@ export class HelpView extends DocumentView {
   isHighlightable = false;
   needsUpdate = false;
 
-  //   static HelpText: string = `Popup and dwim commands
-  //   A Cherry-picking    b Branching         B Bisecting         c Committing
-  //   d Diffing           D Change diffs      e Ediff dwimming    E Ediffing
-  //   f Fetching          F Pulling           l Logging           L Change logs
-  //   m Merging           M Remoting          o Submodules        O Subtrees
-  //   P Pushing           r Rebasing          t Tagging           T Notes
-  //   V Reverting         w Apply patches     W Format patches    X Resetting
-  //   y Show Refs         z Stashing          ! Running           % Worktree
-
-  //  Applying changes
-  //   a Apply          s Stage          u Unstage
-  //   v Reverse        S Stage all      U Unstage all
-  //   k Discard
-
-  //  Essential commands
-  //   g     refresh current buffer
-  //   TAB   toggle section at point
-  //   RET   visit thing at point
-  //   C-h m show all key bindings`;
-
-  static HelpText: string = `Popup and dwim commands
-  A Cherry-pick       b Branch            c Commit
-  d Diff              f Fetch             F Pull
-  i Ignore            l Log               m Merge
-  M Remote            P Push              r Rebase
-  t Tag               V Revert            X Reset
-  y Show Refs         z Stash             ! Run             % Worktree
- 
-Applying changes
-  a Apply          s Stage          u Unstage
-  v Reverse        S Stage all      U Unstage all
-  k Discard
-  
-Essential commands
-  g     refresh current buffer
-  TAB   toggle section at point
-  RET   visit thing at point
-  $     show git process view`;
-
-  constructor(uri: Uri) {
+  constructor(uri: Uri, userKeyBindings: any) {
     super(uri);
 
-    const diffTextView = new TextView(HelpView.HelpText);
+    let binds = meta.contributes.keybindings.reduce((prev, bind) => ({
+      ...prev,
+      [bind.command]: HelpView.keybindingToDisplayKey(bind.key)
+    }), {});
+
+    binds = userKeyBindings.reduce((prev: any, bind: any) => ({
+      ...prev,
+      [bind.command]: HelpView.keybindingToDisplayKey(bind.key)
+    }), binds);
+
+    const diffTextView = new TextView(HelpView.createHelpText(binds));
     diffTextView.isHighlightable = false;
     this.addSubview(diffTextView);
   }
@@ -62,5 +34,50 @@ Essential commands
 
   static encodeLocation(repository: MagitRepository): Uri {
     return Uri.parse(`${Constants.MagitUriScheme}:${HelpView.UriPath}?${repository.rootUri.path}#help`);
+  }
+
+  private static joinTexts(spacing: number, texts: string[]) {
+    let joinedText = texts.length > 0 ? texts[0] : '';
+    for (let i = 1; i < texts.length; i++) {
+      const prev = texts[i - 1];
+      const current = texts[i];
+
+      const remainingSpacing = prev.length <= spacing ? spacing - prev.length : 0;
+      joinedText += ' '.repeat(remainingSpacing) + current;
+    }
+    return joinedText;
+  }
+
+  private static keybindingToDisplayKey(bind: string): string {
+    if (/shift(\+|-)[a-zA-Z]/g.test(bind)) {
+      return bind.substring(6).toLocaleUpperCase();
+    } else if (bind === 'enter') {
+      return 'RET';
+    } else if (bind === 'tab') {
+      return 'TAB';
+    }
+    return bind;
+  }
+
+  private static createHelpText(c: any) {
+    return `Popup and dwim commands
+    ${HelpView.joinTexts(19, [`${c['magit.cherry-picking']} Cherry-pick`, `${c['magit.branching']} Branch`, `${c['magit.commit']} Commit`])}
+    ${HelpView.joinTexts(19, [`${c['magit.diffing']} Diff`, `${c['magit.fetching']} Fetch`, `${c['magit.pulling']} Pull`])}
+    ${HelpView.joinTexts(19, [`${c['magit.ignoring']} Ignore`, `${c['magit.logging']} Log`, `${c['magit.merging']} Merge`])}
+    ${HelpView.joinTexts(19, [`${c['magit.remoting']} Remote`, `${c['magit.pushing']} Push`, `${c['magit.rebasing']} Rebase`])}
+    ${HelpView.joinTexts(19, [`${c['magit.tagging']} Tag`, `${c['magit.reverting']} Revert`, `${c['magit.resetting']} Reset`])}
+    ${HelpView.joinTexts(19, [`${c['magit.show-refs']} Show Refs`, `${c['magit.stashing']} Stash`, `${c['magit.running']} Run`])}
+    ${c['magit.worktree']} Worktree
+
+  Applying changes
+    ${HelpView.joinTexts(17, [`${c['magit.apply-at-point']} Apply`, `${c['magit.stage']} Stage`, `${c['magit.unstage']} Unstage`])}
+    ${HelpView.joinTexts(17, [`${c['magit.reverse-at-point']} Reverse`, `${c['magit.stage-all']} Stage all`, `${c['magit.unstage-all']} Unstage all`])}
+    ${c['magit.discard-at-point']} Discard
+
+  Essential commands
+    ${HelpView.joinTexts(9, [c['magit.refresh'], 'refresh current buffer'])}
+    ${HelpView.joinTexts(9, [c['magit.toggle-fold'], 'toggle section at point'])}
+    ${HelpView.joinTexts(9, [c['magit.visit-at-point'], 'visit thing at point'])}
+    ${HelpView.joinTexts(9, [c['magit.process-log'], 'show git process view'])}`;
   }
 }
