595b59d408c30b1a95ac031a95e67f1b9f53a0a3
Griffin Schneider <griffinschneider@gmail.com>
2020-07-04 04:11:20 -0300

    Highlight branch names in the status header view.

--- src/views/branches/branchHeaderView.ts
+++ src/views/branches/branchHeaderView.ts
@@ -2,13 +2,16 @@ import { View } from '../general/view';
 import { TextView } from '../general/textView';
 import { MagitBranch } from '../../models/magitBranch';
 import GitTextUtils from '../../utils/gitTextUtils';
+import { TokenView } from '../general/tokenView';
 
 export class BranchHeaderView extends View {
 
   constructor(name: string, branch: MagitBranch) {
     super();
     const nameLabel = `${name}:`.padEnd(10);
-    this.addSubview(new TextView(`${nameLabel}${branch.name ?? GitTextUtils.shortHash(branch.commit)} ${GitTextUtils.shortCommitMessage(branch.commitDetails.message)}`));
+    this.addSubview(new TextView(nameLabel));
+    this.addSubview(new TokenView(branch.name ?? GitTextUtils.shortHash(branch.commit), 'magit-ref-name'));
+    this.addSubview(new TextView(` ${GitTextUtils.shortCommitMessage(branch.commitDetails.message)}`));
   }
 
   onClicked() { return undefined; }
--- src/views/branches/remoteBranchHeaderView.ts
+++ src/views/branches/remoteBranchHeaderView.ts
@@ -1,5 +1,6 @@
 import { View } from '../general/view';
 import { TextView } from '../general/textView';
+import { TokenView } from '../general/tokenView';
 import { MagitUpstreamRef } from '../../models/magitBranch';
 import GitTextUtils from '../../utils/gitTextUtils';
 
@@ -8,6 +9,8 @@ export class RemoteBranchHeaderView extends View {
   constructor(name: string, upstreamRef: MagitUpstreamRef) {
     super();
     const nameLabel = `${name}:`.padEnd(10);
-    this.addSubview(new TextView(`${nameLabel}${upstreamRef.remote}/${upstreamRef.name} ${GitTextUtils.shortCommitMessage(upstreamRef.commit?.message)}`));
+    this.addSubview(new TextView(`${nameLabel }`));
+    this.addSubview(new TokenView(`${upstreamRef.remote}/${upstreamRef.name}`, 'magit-remote-ref-name'));
+    this.addSubview(new TextView(` ${GitTextUtils.shortCommitMessage(upstreamRef.commit?.message)}`));
   }
 }
